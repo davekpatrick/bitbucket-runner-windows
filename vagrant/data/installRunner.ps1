ab9ad4c60d602bdac611c2349072692e550b56a5
Dave Patrick <davekpatrick@icloud.com>
2022-06-22 16:10:56 -0400

    updated runner install

--- vagrant/data/installRunner.ps1
+++ vagrant/data/installRunner.ps1
@@ -5,9 +5,10 @@ choco install -y git
 choco install -y openjdk11
 choco install -y dotnetfx --pre
 # download the runner zip
-Invoke-WebRequest -Uri https://product-downloads.atlassian.com/software/bitbucket/pipelines/atlassian-bitbucket-pipelines-runner-1.330.zip -OutFile .\atlassian-bitbucket-pipelines-runner.zip
+cd C:\tmp
+Invoke-WebRequest -Uri https://product-downloads.atlassian.com/software/bitbucket/pipelines/atlassian-bitbucket-pipelines-runner-1.330.zip -OutFile C:\tmp\atlassian-bitbucket-pipelines-runner.zip
 # unzip the file
-Expand-Archive .\atlassian-bitbucket-pipelines-runner.zip
+Expand-Archive C:\tmp\atlassian-bitbucket-pipelines-runner.zip -Force
 #
-cd .\atlassian-bitbucket-pipelines-runner\bin
+cd C:\tmp\atlassian-bitbucket-pipelines-runner\bin
 .\start.ps1 -accountUuid '{8462e43d-34a1-421d-bf72-a16165bb1030}' -runnerUuid '{493861a8-ff17-50b4-ab4d-f2fc4659d693}' -OAuthClientId XHoqLWLKzRL863cZ5ujqX0a53nGozOdV -OAuthClientSecret s3S5DwzyH1DeKJF2F7Uz-AgY1aUI8T5rk0LMOoJRMaEckgiqo6yh_LP4tECdjzPd -workingDirectory '..\temp'
